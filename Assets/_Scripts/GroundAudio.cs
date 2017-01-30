a8a79fe746817a16fbba3417661d4c8d3c7826d8
David Kennedy <davekennedy1990@gmail.com>
2017-01-30 13:03:59 +0000

    Monday 30th January +Added in main menu music +Added in sfx for interacting with the UI +Made it so that ground type affects footstep audio

---
+++ Assets/_Scripts/GroundAudio.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class GroundAudio : MonoBehaviour {
+
+	public List<GroundType> groundTypes = new List<GroundType>();
+	public FirstPersonController FPC;
+	public string currentGround;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit){
+		if (hit.transform.tag == "Snow"){
+			SetGroundType(groundTypes[1]);
+		}else{
+			SetGroundType(groundTypes[0]);
+		}
+	}
+
+	public void SetGroundType(GroundType ground){
+		if(currentGround != ground.name){
+			FPC.m_FootstepSounds = ground.footstepSounds;
+			FPC.m_WalkSpeed = ground.walkSpeed;
+			FPC.m_RunSpeed = ground.runSpeed;
+			currentGround = ground.name;
+		}
+	}
+}
+
+[System.Serializable]
+public class GroundType
+{
+	public string name;
+	public AudioClip[] footstepSounds;
+	public float walkSpeed;
+	public float runSpeed;
+}
